281fa8f387af92e6c2efbd0843b4c6b50eaf0251
Heroin-Bob <thomaspatton2010@yahoo.com>
2025-06-23 00:44:10 -0500

    Update setup reqs.sh

--- setup reqs.sh
+++ setup reqs.sh
@@ -16,7 +16,6 @@ sudo dpkg --add-architecture i386
 sudo add-apt-repository ppa:ubuntu-wine/ppa
 sudo apt-get update
 sudo apt-get install -y wine32
-sudo apt-get install -y wine 
 
 
 echo "Installing Microsoft Fonts:"
