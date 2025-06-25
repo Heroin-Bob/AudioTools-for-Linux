26b247b297e54c593a75a41198c88c6bfd7aa1de
Heroin-Bob <thomaspatton2010@yahoo.com>
2025-06-25 13:43:49 -0500

    Update setup reqs.sh

--- setup reqs.sh
+++ setup reqs.sh
@@ -49,36 +49,6 @@ clear
 echo "Downloading yabridge, moving to appropriate folder, and adding .bashrc path:"
 sleep 3
 
-# Check if curl is installed
-if ! command -v curl &> /dev/null; then
-    echo "curl is not functioning. Exiting the script."
-    exit 1
-fi
-
-curl -s https://api.github.com/repos/robbert-vdh/yabridge/releases/latest \
-| grep "yabridge.*tar.gz" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
-
-find . -name 'yabridge*.tar.gz' -exec tar -xzf {} -C . \;
-find . -name 'yabridge*.tar.gz' -exec rm {} \;
-
-mv ./yabridge $HOME/.local/share
-
-echo -e "\nexport PATH=\"\$PATH:\$HOME/.local/share/yabridge\"" >> ~/.bashrc
-source ~/.bashrc
-
-echo "Making directories..."
-mkdir -p "$HOME/.wine/drive_c/Program Files/Steinberg/VstPlugins" "$HOME/.wine/drive_c/Program Files/Common Files/VST3" "$HOME/.wine/drive_c/Program Files/Common Files/CLAP" "$HOME/Documents/vsts/dll and vst3 files"
-
-yabridgectl add "$HOME/.wine/drive_c/Program Files/Steinberg/VstPlugins"
-yabridgectl add "$HOME/.wine/drive_c/Program Files/Common Files/VST3"
-yabridgectl add "$HOME/.wine/drive_c/Program Files/Common Files/CLAP"
-yabridgectl add "$HOME/Documents/vsts/dll and vst3 files"
-
-echo "Starting yabridge host..."
-$HOME/.local/share/yabridge/yabridge-host.exe
-
-$HOME/.local/share/yabridge/yabridgectl sync
-$HOME/.local/share/yabridge/yabridgectl status
+echo "This setup is now complete."
+echo "You will now need to install yabridge."
+echo "A full yabridge setup script can be found here: https://github.com/Heroin-Bob/AudioTools-for-Linux/blob/main/setup%20yabridge.sh"
