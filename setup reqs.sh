0fd680cc2c005fcc01e1bd2434b26450ae813bbb
Heroin-Bob <thomaspatton2010@yahoo.com>
2025-06-25 13:34:48 -0500

    Update setup reqs.sh

--- setup reqs.sh
+++ setup reqs.sh
@@ -1,10 +1,10 @@
 clear
-echo "Updating and upgrading PC"
-sudo apt update && sudo apt upgrade -y
+# Uncomment the below lines if you want to run updates and upgrades as part of the install process
+#echo "Updating and upgrading PC..."
+# sudo apt update && sudo apt upgrade -y
 
 clear
-sleep 3
-echo "Installing dotnet 8.0"
+echo "Installing dotnet 8.0..."
 sleep 3
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -14,12 +14,12 @@ sudo apt-get install -y aspnetcore-runtime-8.0
 sudo apt-get install -y dotnet-runtime-8.0
 
 clear
-echo "Installing curl"
+echo "Installing curl..."
 sleep 3
 sudo apt-get install -y curl
 
 clear
-echo "Installing Microsoft Fonts:"
+echo "Installing Microsoft Fonts..."
 sleep 3
 sudo apt-get install -y ttf-mscorefonts-installer
 fc-cache -f -v
@@ -32,9 +32,9 @@ sudo apt-get install -y pipewire pipewire-jack pipewire-alsa pipewire-pulse
 
 
 clear
-echo "Installing Wine:"
+echo "Installing Wine..."
 sleep 3
-sudo apt install wine
+sudo apt install -y wine
 sudo apt autoremove
 sudo dpkg --add-architecture i386
 sudo apt-get update
@@ -42,12 +42,19 @@ sudo apt autoremove
 clear
 echo "Installing Wine32:"
 sleep 3
-sudo apt-get install wine32:i386
+sudo apt-get install -y wine32:i386
 wine --version
 sleep 3
 clear
 echo "Downloading yabridge, moving to appropriate folder, and adding .bashrc path:"
 sleep 3
+
+# Check if curl is installed
+if ! command -v curl &> /dev/null; then
+    echo "curl is not functioning. Exiting the script."
+    exit 1
+fi
+
 curl -s https://api.github.com/repos/robbert-vdh/yabridge/releases/latest \
 | grep "yabridge.*tar.gz" \
 | cut -d : -f 2,3 \
