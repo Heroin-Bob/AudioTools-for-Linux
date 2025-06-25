ae60920710169762c325a951428a7ac67059c6a8
Heroin-Bob <thomaspatton2010@yahoo.com>
2025-06-25 15:16:37 -0500

    Create setup yabridge.sh

---
+++ setup/setup yabridge.sh
@@ -0,0 +1,33 @@
+echo "Installing yabridge..."
+
+wget -qO- https://api.github.com/repos/robbert-vdh/yabridge/releases/latest | grep "yabridge.*tar.gz" | cut -d : -f 2,3 | tr -d \" | xargs wget
+
+find . -name 'yabridge*.tar.gz' -exec tar -xzf {} -C . \;
+find . -name 'yabridge*.tar.gz' -exec rm {} \;
+
+mv ./yabridge $HOME/.local/share
+
+echo -e "\nexport PATH=\"\$PATH:\$HOME/.local/share/yabridge\"" >> ~/.bashrc
+source ~/.bashrc
+sleep 5
+echo "Making directories..."
+mkdir -p "$HOME/.wine/drive_c/Program Files/Steinberg/VstPlugins" "$HOME/.wine/drive_c/Program Files/Common Files/VST3" "$HOME/.wine/drive_c/Program Files/Common Files/CLAP" "$HOME/Documents/vsts/dll and vst3 files"
+sleep 5
+yabridgectl add "$HOME/.wine/drive_c/Program Files/Steinberg/VstPlugins"
+yabridgectl add "$HOME/.wine/drive_c/Program Files/Common Files/VST3"
+yabridgectl add "$HOME/.wine/drive_c/Program Files/Common Files/CLAP"
+yabridgectl add "$HOME/Documents/vsts/dll and vst3 files"
+
+echo "Starting yabridge host..."
+$HOME/.local/share/yabridge/yabridge-host.exe
+
+$HOME/.local/share/yabridge/yabridgectl sync
+$HOME/.local/share/yabridge/yabridgectl status
+sleep 3
+clear
+echo 
+echo "This setup is now complete."
+echo 
+yabridgectl --version
+echo
+echo "If you do not see an output reading 'yabridgectl x.x.x' above this line then you will need to reboot your pc and run the yabridgectl setup again."
